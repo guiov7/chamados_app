123a5bc08886cd06f9af1fe21a1fe13fe92d4a6c
guiov7 <guio.developer@gmail.com>
2025-05-01 22:50:33 -0300

    feat: implementando store method em ChamadoController (backend)

--- app/Http/Controllers/ChamadoController.php
+++ app/Http/Controllers/ChamadoController.php
@@ -20,7 +20,10 @@ class ChamadoController extends Controller
      */
     public function create()
     {
-        //
+        $categorias = Categoria::all();
+        $situacoes = Situacao::all();
+
+        return view('chamados.create', compact('categorias', 'situacoes'));
     }
 
     /**
@@ -28,7 +31,33 @@ class ChamadoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validação dos dados do formulário
+        $request->validate([
+            'titulo' => 'required|string|max:255',
+            'categoria_id' => 'required|exists:categorias,id',
+            'descricao' => 'required|string',
+            'prazo_solucao' => 'required|date',
+            /*
+            'situacao_id' automatico backend
+            'data_criacao' automatico backend */
+            'data_solucao' => 'nullable|date',
+        ]);
+
+        // Cria novo Chamado com os dados do form
+        $chamado = new Chamado();
+        $chamado->titulo = $request->input('titulo');
+        $chamado->categoria_id = $request->input('categoria_id');
+        $chamado->descricao = $request->input('descricao');
+        $chamado->prazo_solucao = $request->input('prazo_solucao');
+        $chamado->situacao_id = Situacao::where('nome', 'Novo')->firstOrFail()->id; //situação inicial "Novo"
+        $chamado->data_criacao = now(); // data de criação = datatimeatual
+        $chamado->data_solucao = null; // data inicial nula
+
+        // Salva novo no database
+        $chamado->save();
+
+        // Redireciona para listagem
+        return Redirect::route('chamados.index')->with('success', 'Chamado cadastrado com sucesso!');
     }
 
     /**
